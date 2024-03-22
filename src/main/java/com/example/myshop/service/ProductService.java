13f7edfffcc2328a20a88bc13a99fb9a13899a3f
Abhishek <abhishekagarwal0896@gmail.com>
2024-03-22 23:31:46 +0530

    service file added

---
+++ src/main/java/com/example/myshop/service/ProductService.java
@@ -0,0 +1,11 @@
+package com.example.myshop.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    public void AddProduct(){
+        
+    }
+}
